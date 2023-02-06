3eca835bc218b8885e590d1984196caef1220ef9
jmwb03 <julia.melanie.weber@iem.thm.de>
2023-02-06 21:20:05 +0100

    start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { OnepagerComponent } from './onepager/onepager.component';
+import { HeaderComponent } from './header/header.component';
+
+const routes: Routes = [
+
+  {
+    // Standardroute: Umleitung auf '/home'
+    path: '',
+    redirectTo: 'onepager',
+    pathMatch: 'full'
+},
+{
+  path: 'onepager',
+  component: OnepagerComponent
+},
+{
+  path: 'header',
+  component: HeaderComponent
+},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
